84b74ab186ed7546dc7e940be56f619a6a12fa69
Nax <max.bacoux@gmail.com>
2015-08-07 22:32:42 +0200

    commented the install.sh script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# Get the dotfiles list
 cd ~/.dotfiles/dot
 files=$(find . | cut -c 3-)
 cd
@@ -10,24 +11,32 @@ cd
 echo "Creating a backup folder"
 mkdir -p .dotfiles_backup
 
+# Link each file to home
 for f in $files; do
+  # If the original file is a regular file, then make a backup of it
   if [ -e .$f ] && [ ! -L .$f ] && [ ! -d .$f ]; then
     echo "Creating a backup of .$f"
     mkdir -p .dotfiles_backup/$(dirname $f)
     cp .$f .dotfiles_backup/$f
   fi
+  # If the file is not a directory, then it should be linked
   if [ ! -d .dotfiles/dot/$f ]; then
     rm -rf .$f
     echo "Linking .$f"
     slashes=$(echo .$f | grep -o / | wc -l | tr -d ' ')
     prefix=""
+    # A small hack to make relative symlinks work properly
     if [ $slashes -gt 0 ]; then
       prefix=$(jot -b ../ $slashes | tr -d '\n')
     fi
     ln -s $prefix.dotfiles/dot/$f .$f
   else
+    # If the file is a directory, then create a directory too
+    # This is useful, for example, antigen downloads some stuff in $HOME/.antigen
+    # Creating a directory instead of linking it allow us to cherry-pick what to link
     mkdir -p .$f
   fi
 done
 
 echo "Done !"
+
